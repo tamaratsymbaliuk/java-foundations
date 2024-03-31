2f682625ea192dfcafeebf9ad92c2040efc83e84
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-03-30 22:50:00 -0500

    Classes

--- JF-01-Classes-Objects-Methods/src/Person.java
+++ JF-01-Classes-Objects-Methods/src/Person.java
@@ -1,3 +1,19 @@
+import business.Company;
+
 public class Person {
+    //constant variable uppercase
+    private final double PI = 3.14;
+    private final String MY_BIG_CONSTANT ="My constant";
+    private String firstName;
+    private int age;
+    private long id;
+    private char middleInitial;
+   //creating instance of a class Company and storing it into the variable company
+    static private Company company = new Company();
+    private NewsAgency agency;
+
+    public static void main(String[] args) {
+        System.out.println(company);
+    }
 
 }
