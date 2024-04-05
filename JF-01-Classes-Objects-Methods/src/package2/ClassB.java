8c7eb46bd0a72b2cc133235894b0ce202bc3ee8b
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-04 20:03:31 -0500

    Classes, Objects, Methods

--- JF-01-Classes-Objects-Methods/src/package2/ClassB.java
+++ JF-01-Classes-Objects-Methods/src/package2/ClassB.java
@@ -3,8 +3,23 @@ package package2;
 public class ClassB {
     public void publicMethod(){
         System.out.println("This is a public method");
-
+        privateMethod(); //can be called from  public method of their class
 
     }
+
+    private void privateMethod(){
+        System.out.println("This is the private method");
+    }
+
+    protected void protectedMethod(){ //can be accessed from a different class in the same package or from a different subclass in the different package
+        System.out.println("This is the protected method");
+    }
+
+    void packageProtected(){//more restricted than protected
+        System.out.println("This is the package protected method");
+    }
+
+
+
 
 }
---
+++ JF-01-Classes-Objects-Methods/src/package2/OtherClass.java
@@ -0,0 +1,10 @@
+package package2;
+
+public class OtherClass {
+    public void method1(){
+        ClassB classB = new ClassB();
+        classB.protectedMethod();
+        classB.protectedMethod();
+    }
+
+}
