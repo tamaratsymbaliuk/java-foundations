8d702354fb7a5c837e2f2908a9021f12bafa8f9c
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-18 21:25:52 -0500

    Classes, Objects, Methods

---
+++ JF-01-Classes-Objects-Methods/src/Exercises/ArraysDaysOfTheWeek.java
@@ -0,0 +1,17 @@
+package Exercises;
+
+public class ArraysDaysOfTheWeek {
+
+
+
+    public static void main(String[] args) {
+        String[] daysOfTheWeek = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+        System.out.println("The number of items in the array: " + daysOfTheWeek.length);
+        System.out.println("The 4th day in the array is " + daysOfTheWeek[3]);
+
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+    }
+
+}
