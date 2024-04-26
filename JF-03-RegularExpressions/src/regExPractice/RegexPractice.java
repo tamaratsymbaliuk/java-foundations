afe22d7117f9f60563d6d29e1a02c5c8b82288b3
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-25 21:21:36 -0500

    Regular Expressions

--- JF-03-RegularExpressions/src/regExPractice/RegexPractice.java
+++ JF-03-RegularExpressions/src/regExPractice/RegexPractice.java
@@ -1,4 +1,11 @@
 package regExPractice;
 
 public class RegexPractice {
+    public static void main(String[] args) {
+        System.out.println("Cat".matches("[cC]at")); //true
+        System.out.println("bat".matches("[a-fA-F]at"));//specify range
+        System.out.println("car".matches("[^c]ar")); // ^ except false
+
+    }
+
 }
