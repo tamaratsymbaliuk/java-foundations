8433965906e4d47fe26d9021e65b992ad52ad0c5
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-26 22:00:44 -0500

    Regular Expressions: Pattern, Matcher

--- JF-03-RegularExpressions/src/regExPractice/RegexPractice.java
+++ JF-03-RegularExpressions/src/regExPractice/RegexPractice.java
@@ -1,10 +1,36 @@
 package regExPractice;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexPractice {
     public static void main(String[] args) {
-        System.out.println("Cat".matches("[cC]at")); //true
+        System.out.println("Cat".matches("[cC]at")); //true either c or C
         System.out.println("bat".matches("[a-fA-F]at"));//specify range
         System.out.println("car".matches("[^c]ar")); // ^ except false
+
+        System.out.println("Lat".matches("\\wat")); //string of any first char true
+        System.out.println("G_T".matches("\\w\\w\\w"));//3 chars
+
+        String phoneNumber = "321-333-4567";
+        System.out.println(phoneNumber.matches("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d"));
+        //instead we can use quantifiers
+        String regex = "\\d{3}[-\\s]*\\d{3,}[-\\s]*\\d{3,4}";
+        String phoneNumber1 = "321-333333-        4567";
+
+        System.out.println(phoneNumber1.matches(regex));//still true
+
+
+        Pattern pat = Pattern.compile(regex);
+        Matcher mat = pat.matcher(phoneNumber1);
+
+        if (mat.matches()) {
+
+        }
+
+
+
+
 
     }
 
