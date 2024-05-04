d95dde8dd7b31627cddd031a55cb7756312cc2fc
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-04 15:20:12 -0500

    TranscriptParser

--- JF-03-RegularExpressions/src/TranscriptParser.java
+++ JF-03-RegularExpressions/src/TranscriptParser.java
@@ -13,12 +13,26 @@ public class TranscriptParser {
                 """;
         String regex = """
                 Student\\sNumber:\\s(?<studentNum>\\d{10}).* # Grab student number
+                Grade:\\s+(?<grade>\\d{1,2}).* # Grab grade //+ one or more spaces
+                Birthdate:\\s+(?<month>\\d{2})/(?<day>\\d{2})/(?<year>\\d{4}).* # Grab birthday 
+                Gender:\\s+(?<gender>\\w+)\\b.* # Grab gender
+                State\\sID:\\s+(?<stateID>\\d+)\\b.* # Grab state id
+                Weighted\\)\\s+(?<weightedGPA>[\\d\\.]+)\\b.* # Grab weightedGPA
+                Unweighted\\)\\s+(?<unweightedGPA>[\\d\\.]+)\\b.* # Grab unweightedGPA
                 """;
         Pattern pat = Pattern.compile(regex, Pattern.DOTALL | Pattern.COMMENTS);
         Matcher mat = pat.matcher(transcript);
 
         if (mat.matches()) {
             System.out.println(mat.group("studentNum"));
+            System.out.println(mat.group("grade"));
+            System.out.println(mat.group("month"));
+            System.out.println(mat.group("day"));
+            System.out.println(mat.group("year"));
+            System.out.println(mat.group("gender"));
+            System.out.println(mat.group("stateID"));
+            System.out.println(mat.group("weightedGPA"));
+            System.out.println(mat.group("unweightedGPA"));
 
         }
     }
