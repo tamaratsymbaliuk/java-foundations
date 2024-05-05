ceec4a62ff7c5c858cda445c04115f990e9b96ac
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-04 22:29:14 -0500

    PeopleMatching

---
+++ JF-03-RegularExpressions/src/PeopleMatching.java
@@ -0,0 +1,25 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PeopleMatching {
+    public static void main(String[] args) {
+
+         String people = """
+                Flinstone, Fred, 1/01/1900
+                Rubble, Barney, 2/02/1905
+                Flinstone, Wilma, 3/03/1910
+                Rubble, Betty, 4/4/1915
+                """;
+
+        String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4})\\n";
+        Pattern pat = Pattern.compile(regex);
+        Matcher mat = pat.matcher(people);
+
+        mat.find();
+        System.out.println(mat.group("firstName"));
+        System.out.println(mat.group("lastName"));
+        System.out.println(mat.group("dob"));
+        System.out.println(mat.start("firstName"));
+        System.out.println(mat.end("firstName"));
+    }
+}
