4d7cdb6d617a62fc17477c5f89e88cb272a0eb31
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-14 22:21:48 -0500

    Numbers Math operations

--- JF-04-Numbers/src/MathStuff.java
+++ JF-04-Numbers/src/MathStuff.java
@@ -1,8 +1,30 @@
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class MathStuff {
     public static void main(String[] args) {
         System.out.println(Math.abs(-5)); // 5
         System.out.println(Math.ceil(5.4)); // 6.0
         System.out.println(Math.floor(5.4)); // 5.0
         System.out.println(Math.pow(2,3)); // 8.0
+
+        Random random = new Random();
+        System.out.println(random.nextInt(10));
+
+        SecureRandom secureRandom = new SecureRandom();
+        System.out.println(random.nextInt(10));
+
+        System.out.println(calcAreaOfCircle(3));
     }
+
+    /**
+     * This method calculates the area of a circle
+     * @param radius
+     * @return
+     */
+    public static double calcAreaOfCircle(double radius) {
+        // A = PI * r^2
+        return Math.PI * Math.pow(radius,2);
+    }
+
 }
