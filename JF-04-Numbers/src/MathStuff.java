daf7f7797cb5db3d28818028973f9e6d2ef51353
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-14 22:00:49 -0500

    Numbers Math operations

---
+++ JF-04-Numbers/src/MathStuff.java
@@ -0,0 +1,8 @@
+public class MathStuff {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-5)); // 5
+        System.out.println(Math.ceil(5.4)); // 6.0
+        System.out.println(Math.floor(5.4)); // 5.0
+        System.out.println(Math.pow(2,3)); // 8.0
+    }
+}
