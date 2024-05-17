968952bedaf6b07ea8489a22cc9dcc225c694488
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-16 22:24:00 -0500

    Numbers Math operations

--- JF-04-Numbers/src/NumberStuff.java
+++ JF-04-Numbers/src/NumberStuff.java
@@ -1,3 +1,7 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public class NumberStuff {
     public static void main(String[] args) {
         byte myByte = 0x1f; // in hexadecimal
@@ -11,6 +15,13 @@ public class NumberStuff {
 
         System.out.println(myInt); // 3
         System.out.println(anotherInt); // 7
+
+        BigDecimal num1 = new BigDecimal("2.15");
+        BigDecimal num2 = new BigDecimal("1.10");
+        System.out.println(num1.subtract(num2));
+
+        MathContext mc = new MathContext(5, RoundingMode.HALF_UP);
+        System.out.println(new BigDecimal("5.686").divide(new BigDecimal("4.366"),mc));
 
     }
 }
