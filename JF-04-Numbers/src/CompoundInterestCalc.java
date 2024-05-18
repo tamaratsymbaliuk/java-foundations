d5a949b0a461d54fc6cda51ccecace610410caf3
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-17 22:59:18 -0500

    CompoundInterestCalc

--- JF-04-Numbers/src/CompoundInterestCalc.java
+++ JF-04-Numbers/src/CompoundInterestCalc.java
@@ -1,22 +1,26 @@
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public class CompoundInterestCalc {
-    public static BigDecimal calculate(String principal, String rate, int period, String contribution) {
+    private static final NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormatter = NumberFormat.getPercentInstance();
+    public static BigDecimal calculate(String principal, String rate, int period, String contribution) throws ParseException {
         // Balance(Y) = P(1 + r)^Y + c[ ((1 + r)^Y - 1) / r]
-        BigDecimal a = BigDecimal.ONE.add(new BigDecimal(rate)).pow(period); // (1 + r)^period
+        String rateAsPercent = percentFormatter.parse(rate).toString();
+        BigDecimal a = BigDecimal.ONE.add(new BigDecimal(rateAsPercent)).pow(period); // (1 + r)^period
         BigDecimal c = a.subtract(BigDecimal.ONE); // ((1 + r)^Y - 1)
-        BigDecimal d = c.divide(new BigDecimal(rate)); // ((1 + r)^Y - 1) / r
-        BigDecimal e = d.multiply(new BigDecimal(contribution)); // c[ ((1 + r)^Y - 1) / r]
-        BigDecimal f = a.multiply(new BigDecimal(principal)); // (1 + r)^period * principal
+        BigDecimal d = c.divide(new BigDecimal(rateAsPercent)); // ((1 + r)^Y - 1) / r
+        BigDecimal e = d.multiply(new BigDecimal(moneyFormatter.parse(contribution).toString())); // c[ ((1 + r)^Y - 1) / r]
+        BigDecimal f = a.multiply(new BigDecimal(moneyFormatter.parse(principal).toString())); // (1 + r)^period * principal
         BigDecimal g = f.add(e);
         return g;
 
     }
 
-    public static void main(String[] args) {
-
-        System.out.println( CompoundInterestCalc.calculate("10000.00","0.8",10,"1000"));
+    public static void main(String[] args) throws ParseException {
+        BigDecimal balance = CompoundInterestCalc.calculate("$10000.00","8%",10,"$1000");
+        System.out.println(moneyFormatter.format(balance));
     }
-
-
 }
