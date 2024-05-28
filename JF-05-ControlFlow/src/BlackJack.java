69ca505afae4c4849f21ef2d1cc3ef51037845bd
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-27 21:14:14 -0500

    ControlFlow - BlackJack

---
+++ JF-05-ControlFlow/src/BlackJack.java
@@ -0,0 +1,21 @@
+public class BlackJack {
+    public static void main(String[] args) {
+        String card = "king";
+        int currentTotalValue = 15;
+
+       int currentValue =  switch (card) {
+            case "king", "queen", "jack" -> 10;
+            case "ace" -> {
+                if (currentTotalValue < 11) {
+                    yield 11;
+                } else {
+                    yield 1;
+                }
+            }
+           default -> Integer.parseInt(card);
+
+        };
+        System.out.println(currentValue);
+        System.out.printf("Total value: %d%n", currentTotalValue + currentValue);
+    }
+}
