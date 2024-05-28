ab7259266df7669406d6bb64e4a8c99ecde4e780
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-27 20:48:41 -0500

    ControlFlow - GuessIt Class

--- JF-05-ControlFlow/src/GuessIt.java
+++ JF-05-ControlFlow/src/GuessIt.java
@@ -2,10 +2,10 @@ import java.util.Random;
 
 public class GuessIt {
     public static void main(String[] args) {
-        int randomNum =  new Random().nextInt(3) + 1; //  1 , 2, 3
+        int randomNum =  new Random().nextInt(5) + 1; //  1 , 2, 3, 4 , 5
         System.out.printf("Generated number is: %d.%n", randomNum);
 
-        if (randomNum == 3) {
+        if (randomNum < 2 || (randomNum % 2 == 0)) {
             System.out.println("You got it!");
         } else {
             System.out.println("Sorry, you didn't get it. ");
