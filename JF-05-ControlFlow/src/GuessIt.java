8c99159e4a3f85cd1269bcefbe74432900fb6348
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-30 22:28:16 -0500

    Control Flow GuessIt class

--- JF-05-ControlFlow/src/GuessIt.java
+++ JF-05-ControlFlow/src/GuessIt.java
@@ -3,16 +3,19 @@ import java.util.Random;
 public class GuessIt {
     public static void main(String[] args) {
         int randomNum =  new Random().nextInt(10) + 1; //  1 - 10
+        String guessedNumText = null;
 
-        while (true) {
-            String guessedNumText = System.console().readLine("Please guess a number between 1 and 10: ");
-            int guessedNum = Integer.parseInt(guessedNumText);
+        while (!"quit".equals(guessedNumText)) {
+            guessedNumText = System.console().readLine("Please guess a number between 1 and 10: ");
+            if (guessedNumText.matches("\\d{1,2}")) {
+                int guessedNum = Integer.parseInt(guessedNumText);
 
-            if (guessedNum == randomNum) {
-                System.out.printf("The random number was %d. You got it!%n", randomNum);
-                return;
-            } else {
-                System.out.println("You didn't get it!");
+                if (guessedNum == randomNum) {
+                    System.out.printf("The random number was %d. You got it!%n", randomNum);
+                    return;
+                } else {
+                    System.out.println("You didn't get it!");
+                }
             }
         }
 
