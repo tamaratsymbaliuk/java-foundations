f961b3348226388e51934644b08a00a3ebfc10b9
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-31 18:24:54 -0500

    Control Flow do-while

--- JF-05-ControlFlow/src/GuessIt.java
+++ JF-05-ControlFlow/src/GuessIt.java
@@ -4,10 +4,10 @@ public class GuessIt {
     public static void main(String[] args) {
         int randomNum =  new Random().nextInt(10) + 1; //  1 - 10
         String guessedNumText = null;
-
-        while (!"quit".equals(guessedNumText)) {
+int wrongGuessCount = 0;
+   do {
             guessedNumText = System.console().readLine("Please guess a number between 1 and 10: ");
-            if (guessedNumText.matches("\\d{1,2}")) {
+            if (guessedNumText.matches("-?\\d{1,2}")) {
                 int guessedNum = Integer.parseInt(guessedNumText);
 
                 if (guessedNum == randomNum) {
@@ -17,7 +17,7 @@ public class GuessIt {
                     System.out.println("You didn't get it!");
                 }
             }
-        }
+        }      while (!"quit".equals(guessedNumText));
 
 
 //        System.out.printf("Generated number is: %d.%n", randomNum);
