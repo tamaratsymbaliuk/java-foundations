0bcd9e9037368ca200f74fff8f5781e43203a95e
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-01 23:21:28 +0200

    GuessItForLoop

---
+++ JF-05-ControlFlow/src/GuessItForLoop.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class GuessItForLoop {
+    private static final int MAX_ALLOWED_TRIES = 4;
+
+    public static void main(String[] args) {
+        int randomNum = new Random().nextInt(10) + 1; //  1 - 10
+        String guessedNumText = null;
+        int wrongGuessCount;
+     //   do {
+        for (wrongGuessCount = 1; wrongGuessCount <= MAX_ALLOWED_TRIES; wrongGuessCount++) {
+            guessedNumText = System.console().readLine("Please guess a number between 1 and 10: ");
+            if (guessedNumText.matches("-?\\d{1,2}")) {
+                int guessedNum = Integer.parseInt(guessedNumText);
+
+                if (guessedNum == randomNum) {
+                    String tryText = wrongGuessCount == 1 ? "try" : "tries";
+                    System.out.printf("The random number was %d. You got it in %d %s!%n", randomNum, wrongGuessCount, tryText);
+                    return;
+                } else {
+                    System.out.println("You didn't get it!");
+                }
+            }
+        }
+        //     } while (!"quit".equals(guessedNumText) && wrongGuessCount <= MAX_ALLOWED_TRIES);
+        if (wrongGuessCount >= MAX_ALLOWED_TRIES) {
+            System.out.printf("You've had %d incorrect guesses. The random number is %d. Ending program now.%n", wrongGuessCount - 1, randomNum);
+
+        }
+    }
+}
+
