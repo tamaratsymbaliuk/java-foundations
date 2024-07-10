cba7c2df3b765bf693d2c41f9e07da327f9d9217
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-09 19:14:56 -0500

    More OOP - Rank enum

---
+++ JF-06-More-OOP/src/enums/blackjack/Rank.java
@@ -0,0 +1,18 @@
+package enums.blackjack;
+
+public enum Rank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+}
