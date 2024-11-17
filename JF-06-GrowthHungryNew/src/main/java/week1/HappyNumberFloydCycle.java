decf412a61e256e8e401dbf16a82d14488e880b8
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-11-16 21:40:16 -0600

    Java - Growth Hungry week 1 Happy Number Floyd's Cycle

---
+++ JF-06-GrowthHungryNew/src/main/java/week1/HappyNumberFloydCycle.java
@@ -0,0 +1,29 @@
+package week1;
+
+public class HappyNumberFloydCycle {
+
+    public static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public static boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+
+    public static void main(String[] args) {
+        int n = 19;
+        System.out.println(isHappy(n));
+    }
+}
