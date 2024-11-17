1b48782de3b51ab2cc2d0cc44f4ead1500a2c2b3
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-11-16 21:39:55 -0600

    Java - Growth Hungry week 1 Happy Number

---
+++ JF-06-GrowthHungryNew/src/main/java/week1/HappyNumber.java
@@ -0,0 +1,31 @@
+package week1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public static boolean isHappy(int n) {
+
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+    private static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(isHappy(n));
+    }
+}
+
