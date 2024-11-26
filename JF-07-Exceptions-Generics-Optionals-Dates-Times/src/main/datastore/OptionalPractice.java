1240330dc48a32028d61463508a983935dd04071
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-11-26 12:00:00 -0600

    Optionals

---
+++ JF-07-Exceptions-Generics-Optionals-Dates-Times/src/main/datastore/OptionalPractice.java
@@ -0,0 +1,15 @@
+package main.datastore;
+
+import java.util.Optional;
+
+public class OptionalPractice {
+    public static void main(String[] args) {
+        Optional<String> optMsg = Optional.of("Hello");
+        System.out.println(optMsg);
+
+        String msg2 = null;
+        Optional<String> optMsg2 = Optional.ofNullable(msg2);
+        String finalOutput = optMsg2.orElse("alternative").toUpperCase();
+        System.out.println(finalOutput);
+    }
+}
